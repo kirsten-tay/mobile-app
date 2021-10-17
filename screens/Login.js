5add06a877ce9ee3e31769beae1eca1aea2f3152
Kirsten-tay <taykirsten11@gmail.com>
2021-10-17 00:00:58 +0000

    updated the login page

--- screens/Login.js
+++ screens/Login.js
@@ -11,11 +11,23 @@ export default function Login() {
             backgroundcolor: "white"
             }}>
             <View style={{flex:2}}>
-                <image style={{width:150, height:150, borderRadius:10, marginBottom:20}} 
+                <image style={{width:200, height:200, borderRadius:20, marginBottom:50, transform:[{rotate:"45deg"}],}} 
                 source={{URL:"http"}}/>
             
             <Text style= {{color: "rgba(0,0,0,0,6)", fontSize:24}}> Welcome to </Text>
             <Text style={{color:"black", fontSize: 30, fontWeight:"600" }}>Power Bike Shop</Text>
+            <TouchableOpacity style={{
+                backgroundColor:"#e3e3e3",
+                padding:10,
+                paddingHorizontal:60,
+                flexDirection:"row",
+                alignItems:"center",
+                borderRadius:10,
+                marginTop:20,
+                 }}>
+                     <AntDesign name="google" size={24} color=""
+                 </TouchableOpacity>
+            
             </View>
             <View style={{flex:1, backgroundColor: "red"}}></View>
         </View>
