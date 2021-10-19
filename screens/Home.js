d8447238f716e0e9f72727bc21d8d6273eee70a8
Kirsten-tay <taykirsten11@gmail.com>
2021-10-19 22:37:48 +0000

    updated the home page

--- screens/Home.js
+++ screens/Home.js
@@ -4,7 +4,21 @@ import {View, text} from "react-native"
 
 export default function Home() {
     return(
-        <View>
+        <View style={{
+        backgroundColor:"white",
+        flex:1,
+        paddingTop:55,
+        paddingHorizontal:20,
+        }}>
+            <View style={{
+           flexDirection:"row",
+           justifyContent:"space-between",
+            alignItems:"center",
+            }}>
+                <Ionicons name="ios-menu-outline" size={24} color="black"
+
+            </View>
+
         </View>
     )
 }
