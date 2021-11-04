3fd61f8b71d8de3ac49fa6403a910eaf08881543
Kirsten tay <taykirsten11@gmail.com>
2021-11-04 21:50:11 +0000

    started the cart

--- screens/Cart.js
+++ screens/Cart.js
@@ -2,3 +2,16 @@ import React from 'react'
 import {View, Text, Image, TouchableOpacity, Pressable} from "react-native"
 import {AntDesign} from '@expo/vector-icons';
 
+export default function Home() {
+    return(
+        <View style={{
+        backgroundColor:"white",
+        flex:1,
+        paddingTop:55,
+        paddingHorizontal:20,
+        }}>
+
+        </View>
+    )
+
+}
