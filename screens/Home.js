aa39a55c9c042dce43e61a71e335f65834e66f5b
Kirsten tay <taykirsten11@gmail.com>
2021-11-16 21:28:23 +0000

    updated the home page

--- screens/Home.js
+++ screens/Home.js
@@ -41,7 +41,7 @@ export default function Home() {
                     <Text style={style.categoryItem}>Casual</Text>
            </View>
         </ScrollView>
-        <<View style={{marginTop: 20, flexDirection: 'row', flexWrap: 'wrap', rowGap: 20, columnGap: 20, width: '92%', marginHorizontal: 'auto'}}> {/* width is hacky*/}
+        <View style={{marginTop: 20, flexDirection: 'row', flexWrap: 'wrap', rowGap: 20, columnGap: 20, width: '92%', marginHorizontal: 'auto'}}> {/* width is hacky*/}
             <Card name={"Pinarello"} price={"1700.00"} wishlist={true}/>
             <Card name={"Brompton"} price={"2300.00"} wishlist={false} />
             <Card name={"Schwinn"} price={"5500.00"} wishlist={false}/>
