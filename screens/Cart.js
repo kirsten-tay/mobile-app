04353b7a924269e3ee5e76c119bed41080c15aad
Kirsten tay <taykirsten11@gmail.com>
2021-11-20 22:38:17 +0000

    updated the cart page

--- screens/Cart.js
+++ screens/Cart.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {View, Text, Image, TouchableOpacity, Pressable} from "react-native"
 import {AntDesign} from '@expo/vector-icons';
 
-export default function Home() {
+export default function Cart()) {
     return(
         <View style={{
         backgroundColor:"white",
@@ -10,6 +10,9 @@ export default function Home() {
         paddingTop:55,
         paddingHorizontal:20,
         }}>
+             <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                <Ionicons name="arrow-back" size={24} color="black" />
+                </TouchableOpacity>
             <View style={{
            flexDirection:"row",
            flex:1,
