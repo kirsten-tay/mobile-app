b2e8eb0501d1215e7b81d26a33380a8db43fea88
Kirsten tay <taykirsten11@gmail.com>
2021-12-01 17:46:32 +0000

    added an asset

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {View, Text, Image, StyleSheet, ScrollView} from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
-import BikeProduct  from '../assets/bikee.png';
+import photobike from '../assets/photobike.png';
 
 
 
