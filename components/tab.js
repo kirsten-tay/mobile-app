92805d8fda4af93cc15332eeff5d146a7c0c96bb
Kirsten tay <taykirsten11@gmail.com>
2021-12-01 17:54:40 +0000

    updated the tab

--- components/tab.js
+++ components/tab.js
@@ -5,3 +5,13 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export  default  function({homeActive}){
     return (
+        <View style={{flexDirection: 'row', justifyContent: 'space-between', backgroundColor: "#e6e6e6", position: 'fixed', bottom: 0, left: 0, right: 0, paddingVertical: 20, paddingHorizontal: '15%'}}>
+    <Entypo name="home" size={24} color={homeActive ? '#f17827' : '#a3a3a3'} />
+    <View style={{position: 'absolute', justifyContent: 'center', alignItems: 'center', top: '-50%', left: '50%', transform: [{translate: '-50%'}], backgroundColor: '#000', borderRadius: '50%', width: 65, height: 65}}>
+        <MaterialCommunityIcons name="microphone-outline" size={24} color="white"/>
+    </View>
+    <SimpleLineIcons name="handbag" size={24} color={!homeActive ? '#f17827' : '#a3a3a3'} />
+
+</View>
+)
+}
