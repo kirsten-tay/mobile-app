a90d01b3bb6a8483f3a0eda8dfc2d734fc5b8e24
Kirsten tay <taykirsten11@gmail.com>
2021-12-27 23:37:03 +0000

    updated the home

--- components/tab.js
+++ components/tab.js
@@ -7,14 +7,9 @@ import { NavigationContainer } from '@react-navigation/native';
 export  default  function({homeActive}){
     return (
         <View style={{flexDirection: 'row', justifyContent: 'space-between', backgroundColor: "#e6e6e6", position: 'fixed', bottom: 0, left: 0, right: 0, paddingVertical: 20, paddingHorizontal: '15%'}}>
-    <TouchableOpacity 
-                onPress={() => {
-                   navigation.navigate("Home")
-                }}
-                activeOpacity={0,8}
-                >
+    
     <Entypo name="home" size={24} color={homeActive ? '#f17827' : '#a3a3a3'} />
-    </TouchableOpacity>
+    
     <View style={{position: 'absolute', justifyContent: 'center', alignItems: 'center', top: '-50%', left: '50%', transform: [{"translate": "-50%"}], backgroundColor: '#000', borderRadius: '50%', width: 65, height: 65}}>
         <MaterialCommunityIcons name="microphone-outline" size={24} color="white"/>
     </View>
