73e486eb45c881d47cddb21d63ae861cd1cdeb2d
Kirsten tay <taykirsten11@gmail.com>
2021-12-27 23:36:32 +0000

    updated the home

--- components/tab.js
+++ components/tab.js
@@ -18,14 +18,10 @@ export  default  function({homeActive}){
     <View style={{position: 'absolute', justifyContent: 'center', alignItems: 'center', top: '-50%', left: '50%', transform: [{"translate": "-50%"}], backgroundColor: '#000', borderRadius: '50%', width: 65, height: 65}}>
         <MaterialCommunityIcons name="microphone-outline" size={24} color="white"/>
     </View>
-    <TouchableOpacity 
-                onPress={() => {
-                   navigation.navigate("Cart")
-                }}
-                activeOpacity={0,8}
-                >
+    city={0,8}
+                
     <SimpleLineIcons name="handbag" size={24} color={!homeActive ? '#f17827' : '#a3a3a3'} />
-     </TouchableOpacity>
+    
 </View>
 )
 }
