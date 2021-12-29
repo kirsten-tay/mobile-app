4fdeebecf2d9190f2f7760d3fb9f2489f1b234a9
Kirsten tay <taykirsten11@gmail.com>
2021-12-29 22:37:47 +0000

    updated the cart

--- screens/Cart.js
+++ screens/Cart.js
@@ -50,7 +50,7 @@ export default function Cart({navigation}) {
                 </View>
             </View>
 
-            <TouchableOpacity style={{paddingVertical: 15, paddingHorizontal: 50, backgroundColor: '#fa6401', borderRadius: 5, width: 'min-content', marginHorizontal: 'auto', marginTop: 5, marginBottom:110}}>
+            <TouchableOpacity style={{paddingVertical: 15, paddingHorizontal: 50, backgroundColor: '#fa6401', borderRadius: 5, width: 'min-content', marginHorizontal: 'auto', marginTop: 5, marginBottom:120}}>
                 <Text style={{fontSize: 18,fontWeight: 700, textAlign: 'center', color: 'white', whiteSpace: 'nowrap'}}>Proceed to Checkout</Text>
             </TouchableOpacity>
             <Tab homeActive={false} />
