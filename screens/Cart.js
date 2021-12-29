42c0bcc5ea80fa3bd46834ef9403f63f38f79510
Kirsten tay <taykirsten11@gmail.com>
2021-12-29 23:02:46 +0000

    updated the cart

--- screens/Cart.js
+++ screens/Cart.js
@@ -24,8 +24,8 @@ export default function Cart({navigation}) {
            justifyContent:"space-between",
             alignItems:"center",
             }}>
-                <Text style={{fontWeight: 700, fontSize: 20, color: 'rgba(0,0,0, .8)'}}>Cart list</Text>
-                    <Text style={{fontWeight: 600, fontSize: 15, color: 'rgba(0,0,0, .4)', textAlign: 'center'}}>(3 items)</Text>
+                <Text style={{fontWeight: "700", fontSize: 20, color: 'rgba(0,0,0, .8)'}}>Cart list</Text>
+                    <Text style={{fontWeight: "600", fontSize: 15, color: 'rgba(0,0,0, .4)', textAlign: 'center'}}>(3 items)</Text>
             </View>
             </View>
 
@@ -37,21 +37,21 @@ export default function Cart({navigation}) {
 
     <View style={{backgroundColor: 'rgba(0,0,0,.05)', borderRadius: 15, paddingVertical: 20, paddingHorizontal: 20, marginVertical: 10}}>
                 <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                    <Text style={{fontSize: 17, color: 'rgba(0,0,0,.5)', fontWeight: 500}}>Subtotal</Text>
-                    <Text style={{fontSize: 17, fontWeight: 600, color: '#f17827'}}>$<Text style={{color: '#000', fontSize: 18}}>6,566.69</Text></Text>
+                    <Text style={{fontSize: 17, color: 'rgba(0,0,0,.5)', fontWeight: "500"}}>Subtotal</Text>
+                    <Text style={{fontSize: 17, fontWeight: "600", color: '#f17827'}}>$<Text style={{color: '#000', fontSize: 18}}>6,566.69</Text></Text>
                 </View>
                 <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
                     <Text style={{fontSize: 17, color: 'rgba(0,0,0,.5)', fontWeight: 500}}>Shipping</Text>
-                    <Text style={{fontSize: 17, fontWeight: 600, color: '#f17827'}}>$<Text style={{color: '#000', fontSize: 18}}>1,000.00</Text></Text>
+                    <Text style={{fontSize: 17, fontWeight: "600", color: '#f17827'}}>$<Text style={{color: '#000', fontSize: 18}}>1,000.00</Text></Text>
                 </View>
                 <View style={{flexDirection: 'row', justifyContent: 'space-between', marginVertical: 15, borderTop: '2px dashed rgba(0,0,0,.4)', paddingTop: 10}}>
-                    <Text style={{fontSize: 17, fontWeight: 700}}>Total</Text>
-                    <Text style={{fontSize: 17, fontWeight: 700, color: '#f17827'}}>$ <Text style={{color: '#000', fontSize: 18}}>7,566.69.00</Text></Text>
+                    <Text style={{fontSize: 17, fontWeight: "700"}}>Total</Text>
+                    <Text style={{fontSize: 17, fontWeight: "700", color: '#f17827'}}>$ <Text style={{color: '#000', fontSize: 18}}>7,566.69.00</Text></Text>
                 </View>
             </View>
 
             <TouchableOpacity style={{paddingVertical: 15, paddingHorizontal: 50, backgroundColor: '#fa6401', borderRadius: 5, width: 'min-content', marginHorizontal: 'auto', marginTop: 5, marginBottom:120}}>
-                <Text style={{fontSize: 18,fontWeight: 700, textAlign: 'center', color: 'white', whiteSpace: 'nowrap'}}>Proceed to Checkout</Text>
+                <Text style={{fontSize: 18,fontWeight: "700", textAlign: 'center', color: 'white', whiteSpace: 'nowrap'}}>Proceed to Checkout</Text>
             </TouchableOpacity>
             <Tab homeActive={false} />
   </View>
