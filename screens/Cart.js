7e128274d0825f41f8837081e1f57ad2b8c3b805
Kirsten tay <taykirsten11@gmail.com>
2022-01-03 23:28:04 +0000

    updated the cart and cartitem

--- screens/Cart.js
+++ screens/Cart.js
@@ -14,19 +14,17 @@ export default function Cart({navigation}) {
         flex:2,
         paddingTop:55,
         paddingHorizontal:10,
-        justifyContent:'space-between'
         }}>
              <TouchableOpacity onPress={() => navigation.navigate('Home')}>
                 <Ionicons name="arrow-back" size={24} color="black" />
                 </TouchableOpacity>
-                <Text style={{fontWeight: "700", fontSize: 20, color: 'rgba(0,0,0, .8)'}}>Cart list</Text>
             <View style={{
-           flexDirection:"row",
+           flexDirection:"column",
            flex:1,
            justifyContent:"center",
             alignItems:"center",
             }}>            
-
+                <Text style={{fontWeight: "700", fontSize: 20, color: 'rgba(0,0,0, .8)'}}>Cart list</Text>
                     <Text style={{fontWeight: "600", fontSize: 15, color: 'rgba(0,0,0, .4)', textAlign: 'center'}}>(3 items)</Text>
             </View>
             </View>
