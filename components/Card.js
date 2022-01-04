f08edf83aaa885b5f19fded3da5140d00e11c0da
Kirsten tay <taykirsten11@gmail.com>
2022-01-04 22:43:06 +0000

    updated the cart and cartitem

--- components/Card.js
+++ components/Card.js
@@ -8,14 +8,17 @@ import photobike from '../assets/photobike.png';
 
 const Card = ({name, price, wishlist}) => {
     return (
+        <ScrollView       contentContainerStyle={styles.scrollContentContainer} >
+
     <View style={styles.card}>
-        <View style={styles.wishlist}>
-            <AntDesign name="hearto" size={20} color={wishlist ? "#f17827" : "#a3a3a3"} style={{position: 'absolute', top: '50%', left:'50%', transform: [{"translate": "-50, -50"}]}}/>
+        <View style={styles.wishlist, {transform: [{translate:-50}] }}>
+            <AntDesign name="hearto" size={20} color={wishlist ? "#f17827" : "#a3a3a3"} style={ {position:'absolute' , top: '50%', left:'50%'} }/>
         </View>
         <Image source={photobike} style={{width: 150, height: 100, marginTop: 30, marginBottom: 10}}/>
         <Text style={{color: 'rgba(0,0,0, .4)', fontWeight: "500", textAlign: 'center', fontSize: 17}}>{name} Bike</Text>
         <Text style={{color: '#f17827', fontWeight: "700", fontSize: 17, textAlign: 'center'}}>$<Text style={{color: 'rgba(0,0,0, .8)', fontSize: 20}}>{price}</Text></Text>
     </View>
+    </ScrollView>
     )
 }
 
