0359e39f82e254982a8d9e2d0dea7896feed486d
Kirsten tay <taykirsten11@gmail.com>
2022-01-04 22:24:24 +0000

    updated the cart and cartitem

--- components/Card.js
+++ components/Card.js
@@ -10,7 +10,7 @@ const Card = ({name, price, wishlist}) => {
     return (
     <View style={styles.card}>
         <View style={styles.wishlist}>
-            <AntDesign name="hearto" size={20} color={wishlist ? "#f17827" : "#a3a3a3"} style={{position: 'absolute', top: '50%', left:'50%', Transform: [{"translate": "-50%, -50%"}]}}/>
+            <AntDesign name="hearto" size={20} color={wishlist ? "#f17827" : "#a3a3a3"} style={{position: 'absolute', top: '50%', left:'50%', transform: [{"translate": "-50, -50"}]}}/>
         </View>
         <Image source={photobike} style={{width: 150, height: 100, marginTop: 30, marginBottom: 10}}/>
         <Text style={{color: 'rgba(0,0,0, .4)', fontWeight: "500", textAlign: 'center', fontSize: 17}}>{name} Bike</Text>
