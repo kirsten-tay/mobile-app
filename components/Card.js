46cc1c2300ccab3d9f7b93312ea9d957baac9aa4
Kirsten tay <taykirsten11@gmail.com>
2022-01-05 23:01:45 +0000

    updated the cart and cartitem

--- components/Card.js
+++ components/Card.js
@@ -11,7 +11,7 @@ const Card = ({name, price, wishlist}) => {
         <ScrollView       contentContainerStyle={styles.scrollContentContainer} >
 
     <View style={styles.card}>
-        <View style={styles.wishlist, {transform: [{translate:-50}] }}>
+        <View style={styles.wishlist, {transform: [{translate:"-50%"}] }}>
             <AntDesign name="hearto" size={20} color={wishlist ? "#f17827" : "#a3a3a3"} style={ {position:'absolute' , top: '50%', left:'50%'} }/>
         </View>
         <Image source={photobike} style={{width: 150, height: 100, marginTop: 30, marginBottom: 10}}/>
